38aba87886f8ff1917fb589eee89bff4acc12679
FelipeMDantas <felipemdantas@terra.com.br>
2023-05-10 23:49:17 -0300

    Tailwind further configured and Button component added.

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Input from "@/app/components/inputs/Input";
+import Button from "@/app/components/Button";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
@@ -38,8 +39,44 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input />
+          {variant === "REGISTER" && (
+            <Input label="name" register={register} id="name" errors={errors} />
+          )}
+          <Input
+            label="Email Address"
+            register={register}
+            id="email"
+            errors={errors}
+            type="email"
+          />
+          <Input
+            label="Password"
+            register={register}
+            id="password"
+            errors={errors}
+            type="password"
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2"></div>
+        </div>
       </div>
     </div>
   );
